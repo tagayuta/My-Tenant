e8ee1eb3cf5ac0a99030d1f5f2565943131b01e5
y-taga <yt23044@athome.co.jp>
2023-06-16 09:51:41 +0900

    画像編集が完成

--- admin/exeEditProduct.php
+++ admin/exeEditProduct.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+    $imgList = $_SESSION["imgList"];
     $arr = $_FILES["image"]['name'];
     $product_id = $_POST["product_id"];
     $name = $_POST["name"];
@@ -9,6 +11,8 @@
     $dsn = "mysql:host=localhost;dbname=Tenant;charset=utf8";
     $user = "root";
     $pass = "";
+
+    $noImage = "noImage.jpg";
 
     try{
         $db = new PDO($dsn,$user,$pass);
@@ -22,23 +26,22 @@
         $stmt->bindParam(5, $product_id);
         $stmt->execute();
 
-        foreach($arr as $img) {
-            if($img == "") {
-                $SQL = "UPDATE images SET imgPass = ? WHERE product_id = ?";
-                $stmt = $db->prepare($SQL);
-                $stmt->bindParam(1, "noImage.jpg");
-                $stmt->bindParam(2, $product_id);
+        for($i = 0; $i < count($arr); $i++) {
+            if($arr[$i] == "") {
+                continue;
             } else {
+                $img = $arr[$i];
+                $p_id = $imgList[$i][1];
                 $SQL = "UPDATE images SET imgPass = ? WHERE img_id = ?";
                 $stmt = $db->prepare($SQL);
                 $stmt->bindParam(1, $img);
-                $stmt->bindParam(2, $product_id);
+                $stmt->bindParam(2, $p_id);
+                $stmt->execute();
             }
-
-            $stmt->execute();
         }
 
         header("Location: adminIndex.php");
+        exit();
     } catch(PDOException $e) {
         echo "エラー内容：".$e->getMessage();
         die();
